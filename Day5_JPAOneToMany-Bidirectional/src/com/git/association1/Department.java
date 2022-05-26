cc6c7cbbe2410cdbb515f4417cfee0650450dc61
sanju <sanju@DileepKumar>
2022-05-26 19:45:22 +0530

    Second Commit

--- Day5_JPAOneToMany-Bidirectional/src/com/git/association1/Department.java
+++ Day5_JPAOneToMany-Bidirectional/src/com/git/association1/Department.java
@@ -1,5 +1,28 @@
 package com.git.association1;
 
-public class Department {
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="department")
+public class Department 
+{
+	@Id
+	private int id;
+	private String name;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
